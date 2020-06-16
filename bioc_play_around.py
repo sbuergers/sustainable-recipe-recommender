24dc713a665ec70f61b6c31d81904046e3f2f4c9
Steffen Buergers <sbuergers@gmail.com>
2020-06-16 15:38:04 -0400

    renamed create_ingredient_dataframe.py to process_crf_data.py

--- bioc_play_around.py
+++ bioc_play_around.py
@@ -5,3 +5,29 @@ Created on Wed Jun 10 09:13:38 2020
 @author: sbuer
 """
 
+import os
+import bioc
+
+
+# Deserialize ``fp`` to a BioC collection object.
+pathname = 'foodbase'
+filename = 'FoodBase_uncurated'
+suffix = '.xml'
+with open(os.path.join(pathname, filename + suffix), 'r') as fp:
+    collection = bioc.load(fp)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
