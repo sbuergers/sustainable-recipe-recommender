fe91903c6c9f553787434f3210694a0d8369f3e0
Steffen Buergers <sbuergers@gmail.com>
2020-06-16 15:26:25 -0400

    first draft script of imputing ingredient values where missing

--- impute_ingredient_ghg_values.py
+++ impute_ingredient_ghg_values.py
@@ -44,101 +44,6 @@ df[np.logical_and(df['ghg_missing']==1, # No GHG emission estimate
 				  df['name_man_pruned2'].notna())] # Has GHG look-up label
 
 
-##
-## ++++++ Finding missing Units ++++++
-##
-## In most cases, when a unit is missing, it relates to single scallions,
-## onions, bananas, etc.
-items = df[np.logical_and(df['ghg_missing']==1, # No GHG emission estimate
-				  df['name_man_pruned2'].notna())]['name_man_pruned1'].unique()
-print(items)
-
-## Try to find the most common unit based on the first pruned name and use as
-## unit:
-single_items = {}
-items_not_found = []
-for item in items:
-	try:
-		single_items[item] = df['unit_man'][df['name_man_pruned1'] == item].value_counts().keys()[0]
-	except:
-		items_not_found.append(item)
-
-print('Could not assign units to the following ingredient labels (name_man_pruend1):')
-print(items_not_found)
-
-## Assign the missing ones manually:
-## Listed in 'create_ingredient_dataframe.py
-single_items['rice oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
-single_items['ball burrata'] = 'ounce'
-single_items['maple leave'] = 'tablespoon'
-single_items['corncob'] = 'cup'
-single_items['cilantro oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
-single_items['soybean oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
-single_items['pheasant'] = 'pound'
-single_items['locust'] = 'coffeespoon'
-single_items['chips'] = 'cup'
-single_items['barramundi'] = 'teacup'
-single_items['raspberry'] = 'ounce'
-single_items['giblet'] = 'pound'
-single_items['bagel'] = 'teacup'
-single_items['sweet potato'] = 'teacup'
-single_items['taco'] = 'wineglass'
-single_items['hominy'] = 'ounce'
-single_items['matzoh'] = 'wineglass'
-single_items['herring'] = 'teacup'
-single_items['frico'] = 'teacup'
-single_items['langoustine'] = 'teacup'
-single_items['warp'] = 'teacup'
-single_items['gingersnap'] = 'teacup'
-
-
-## Some more fine grained adjustments:
-df.loc[113449, 'qty_man'] = 6
-df.loc[59073, 'qty_man'] = 3
-df.loc[221028, 'qty_man'] = 2.75
-df.loc[285549, 'qty_man'] = 2
-df.loc[296729, 'qty_man'] = 2.25
-df.loc[348169, 'qty_man'] = 2.25
-
-
-## Remove keys that consist of numbers (comes from a mistake I made in 
-## create_ingredient_dataframe.py - have to rerun)
-number_keys = []
-for key in single_items.keys():
-	if key[0] in '0123456789':
-		number_keys.append(key)
-	print(key)
-
-for delkey in number_keys:
-	single_items.pop(delkey)
-
-
-## Insert unit estimates to df
-for key in single_items.keys():
-	df.loc[df['name_man_pruned1']==key,'unit_man'] = single_items[key]
-
-
-
-## Save updated data
-df.to_csv(r'D:\data science\nutrition\ingredients_manually_processed2.csv')
-
-
-
-
-## Let's have a look at the remaining rows without units
-print('There are now', df[df['unit_man'].isnull()].shape[0], 'rows without units')
-df[df['unit_man'].isnull()]
-
-
-## The vast majority of these are spices
-df[df['unit_man'].isnull()]['name_man_pruned1'].value_counts()
-
-## As most spices probably do not contribute that much let's leave them for now
-
-
-
-
-
 
 
 ##
@@ -248,6 +153,7 @@ def update_ghg(d, ghg):
 	LIST	   
 	with ghg emissions where they could be estimated for each row in d
 	'''
+	from units import units_ml
 	ghg_list = list()
 	for idx in d.index:
 		
@@ -256,7 +162,7 @@ def update_ghg(d, ghg):
 		
 		try:
 			# convert "fractions" to decimals before converting str to float
-			numstr = d.iloc[idx]['qty_man']
+			numstr = d.loc[idx, 'qty_man']
 			if '/' in numstr:
 				numL = numstr.split(sep='/')
 				q = float(numL[0]) / float(numL[1])
@@ -267,13 +173,13 @@ def update_ghg(d, ghg):
 		
 		# convert quantity to ml (which is also roughly grams)
 		try:
-			qtotal = q * units_ml[d.iloc[idx]['unit_man']]
+			qtotal = q * units_ml[d.loc[idx, 'unit_man']]
 		except:
 			qtotal = 0
 			
 		# Estimate GHG emissions
 		ghg_val = qtotal * 0.001 * ghg['Total'][ghg['Food product'] == 
-								   d.iloc[idx]['name_man_pruned2']].values
+										  d.loc[idx, 'name_man_pruned2']]
 		
 		if ghg_val.size == 0:
 			ghg_val = 0
@@ -286,6 +192,157 @@ def update_ghg(d, ghg):
 
 # Find ghg emission estimates for imputed values
 ghg_list = update_ghg(df, ghg)
+df['ghg_new'] = ghg_list
+
+
+# Visualize new and old ghg estimate distribution
+import matplotlib.pyplot as plt
+import seaborn as sns
+log_ghg = np.log(df["ghg"])
+log_ghg_new = np.log(df['ghg_new'])
+sns.distplot(log_ghg[log_ghg > -100], color="skyblue", label="ghg")
+sns.distplot(log_ghg_new[log_ghg_new > -100], color="red", label="ghg_new")
+
+
+
+
+
+
+
+
+
+##
+## ++++++ Finding missing Units ++++++
+##
+## In most cases, when a unit is missing, it relates to single scallions,
+## onions, bananas, etc.
+items = df[np.logical_and(df['ghg_missing']==1, # No GHG emission estimate
+				  df['name_man_pruned2'].notna())]['name_man_pruned1'].unique()
+print(items)
+
+
+## Assigning the most common unit does not work well - 
+## you end up with 100 pounds of beef for example, while it should be 100 g
+
+
+
+## ---- old: substitute unit with most common unit -----
+
+# ## Try to find the most common unit based on the first pruned name and use as
+# ## unit:
+# single_items = {}
+# items_not_found = []
+# for item in items:
+# 	try:
+# 		single_items[item] = df['unit_man'][df['name_man_pruned1'] == item].value_counts().keys()[0]
+# 	except:
+# 		items_not_found.append(item)
+
+# print('Could not assign units to the following ingredient labels (name_man_pruend1):')
+# print(items_not_found)
+
+
+# ## Assign the missing ones manually:
+# ## Listed in 'create_ingredient_dataframe.py
+# single_items['rice oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
+# single_items['ball burrata'] = 'ounce'
+# single_items['maple leave'] = 'tablespoon'
+# single_items['corncob'] = 'cup'
+# single_items['cilantro oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
+# single_items['soybean oil'] = df['unit_man'][df['name_man_pruned1'] == 'olive oil'].value_counts().keys()[0]
+# single_items['pheasant'] = 'pound'
+# single_items['locust'] = 'coffeespoon'
+# single_items['chips'] = 'cup'
+# single_items['barramundi'] = 'teacup'
+# single_items['raspberry'] = 'ounce'
+# single_items['giblet'] = 'pound'
+# single_items['bagel'] = 'teacup'
+# single_items['sweet potato'] = 'teacup'
+# single_items['taco'] = 'wineglass'
+# single_items['hominy'] = 'ounce'
+# single_items['matzoh'] = 'wineglass'
+# single_items['herring'] = 'teacup'
+# single_items['frico'] = 'teacup'
+# single_items['langoustine'] = 'teacup'
+# single_items['warp'] = 'teacup'
+# single_items['gingersnap'] = 'teacup'
+
+
+## Some more fine grained adjustments:
+df.loc[113449, 'qty_man'] = 6
+df.loc[59073, 'qty_man'] = 3
+df.loc[221028, 'qty_man'] = 2.75
+df.loc[285549, 'qty_man'] = 2
+df.loc[296729, 'qty_man'] = 2.25
+df.loc[348169, 'qty_man'] = 2.25
+
+
+# ## Remove keys that consist of numbers (comes from a mistake I made in 
+# ## create_ingredient_dataframe.py - have to rerun)
+# number_keys = []
+# for key in single_items.keys():
+# 	if key[0] in '0123456789':
+# 		number_keys.append(key)
+# 	print(key)
+
+# for delkey in number_keys:
+# 	single_items.pop(delkey)
+
+
+# ## Insert unit estimates to df 
+# for key in single_items.keys():
+# 	df.loc[(df['name_man_pruned1']==key) & (df['unit_man'].isnull()),'unit_man'] = single_items[key]
+
+
+
+## Instead, try to predict the ghg value based on other entries with the same
+## label (and a valid ghg value). Use kNN based on qty within item labels with 
+## ghg. E.g. when looking for egg, check all rows with egg entries in
+## name_man_pruned1 and ghg values > 0 and select the ones with quantity
+## values closest to the current one if it exists.
+df['ghg_new'] = df['ghg']
+for i, item in enumerate(items):
+	
+	# Get existing ghg estimates
+	y = df.loc[(df['name_man_pruned1']==item) & (df['ghg']>0),'ghg']
+	
+	# Get median
+	medy = np.median(y)
+	print(i, 'of', len(items), 'Imputing ghg emissions for', item, 
+	   '--> Median=', medy)
+		
+	# Replace missing ghg estimates with median ghg estimate
+	df.loc[(df['name_man_pruned1']==item) & (df['ghg']==0),'ghg_new'] = medy
+
+
+
+## Save updated data
+df.to_csv(r'D:\data science\nutrition\ingredients_manually_processed2.csv')
+
+
+
+
+## Let's have a look at the remaining rows without units
+print('There are now', df[df['unit_man'].isnull()].shape[0], 'rows without units')
+df[df['unit_man'].isnull()]
+
+
+
+
+
+# Visualize new and old ghg estimate distribution
+import matplotlib.pyplot as plt
+import seaborn as sns
+log_ghg = np.log(df["ghg"])
+log_ghg_new = np.log(df['ghg_new'])
+sns.distplot(log_ghg[log_ghg > -100], color="skyblue", label="ghg")
+sns.distplot(log_ghg_new[log_ghg_new > -100], color="red", label="ghg_new")
+
+
+
+df[df['ghg_new'] > 300]
+
+
 
 
 # Save data frame
@@ -357,7 +414,12 @@ df_rec = pd.read_csv(r'D:\data science\nutrition\epi_recipes_clean.csv')
 
 
 # Find matches
-for missing_ingr in tmp.keys():
+missing_ingr_not_found = []
+for i, missing_ingr in enumerate(tmp.keys()):
+	
+	# Maybe I cannot find a match, then the estimate remains 0
+	est_ghg = 0
+	
 	# Go through recipes and find exact matches and contained matches 
 	# (exact: mayonnaise, mayonnaise; contained: mayonnaise, sandwich with 
 	# mayonnaise)
@@ -371,8 +433,37 @@ for missing_ingr in tmp.keys():
 			
 	# If exact match exists (there can only be one), use that
 	if recipe_match:
-		est_ghg = 
-		print('Found exact match for', missing_ingr, 'estimated ghg is', est_ghg)
+		est_ghg = df.loc[recipe_match[0],'ghg']
+		print(i, 'Found exact match for', missing_ingr, '- estimated ghg is:', est_ghg)
+	# Otherwise compute average over partial matches
+	elif recipe_in:
+		est_ghg = np.mean(df.loc[recipe_in,'ghg'])
+		print(i, 'Found contained match(es) for', missing_ingr, '- estimated ghg is:', est_ghg)	
+		
+	# Assign estimated ghg emission
+	if est_ghg == 0:
+		print(i, '!!! Could not find any match for', missing_ingr)
+		missing_ingr_not_found.append(missing_ingr)
+	else:	
+		df.loc[df['name_man_pruned1']==missing_ingr,'ghg_new'] = est_ghg
+
+print('Could not find', len(missing_ingr_not_found), 'out of', len(tmp))
+print(missing_ingr_not_found)
+
+
+
+
+
+# Save data frame
+df.to_csv(r'D:\data science\nutrition\ingredients_manually_processed2.csv')
+
+
+
+
+
+
+# Load in again
+df = pd.read_csv(r'D:\data science\nutrition\ingredients_manually_processed2.csv')
 
 
 
@@ -381,66 +472,4 @@ for missing_ingr in tmp.keys():
 
 
 
-
-## Try to find the most common unit based on the first pruned name and use as
-## unit:
-for 
-
-## In some cases I might be able to infer the unit or quantity based on what
-## is saved in comment, other or, indeed, in the original ingredient entry
-
-## Start by looking for units in 
-## 1.) comment,
-## 2.) other,
-## 3.) input
-
-
-## Are there some where we can use the 'other' column to help?
-df[np.logical_and(df.index.isin(ex), df['other'].notnull().values)]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## EOF
+## eof
